d71f64ed0672e824f369b30587c8cb59828e0040
Chelsea <chelsea@tripwiretech.com>
2023-07-10 14:04:44 +1200

    Initial Commit

---
+++ ButtonTutorial/ButtonTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTutorialApp.swift
+//  ButtonTutorial
+//
+//  Created by Charles Suddens on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
